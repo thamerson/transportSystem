ce12b697752a42cc9d0b5970aa7a40f2cab4be1c
Higor Stefany dos santos nóbrega <higorst.nobrega@gmail.com>
2018-09-24 17:36:03 -0300

    sending the project

---
+++ src/main/java/br/ufrn/ceres/bsi/poo2es2/transportSystem/App.java
@@ -0,0 +1,13 @@
+package br.ufrn.ceres.bsi.poo2es2.transportSystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
